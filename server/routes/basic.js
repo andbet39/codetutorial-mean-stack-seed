8f23df3ad71dec1c6a269540e83cd01122d7650c
Andrea Terzani <andrea.terzani@gmail.com>
2015-03-31 18:44:34 +0200

    Grunt Livereload and connect added

--- server/routes/basic.js
+++ server/routes/basic.js
@@ -2,13 +2,12 @@ var path = require('path');
 
     module.exports = function(app) {
 
-        // basic routes to handle request
-        app.get('*', function(req, res) {
+       // basic routes to handle request
+        app.get('/', function(req, res) {
                 console.log("Main route");
                 res.sendFile(path.join(__dirname, '../../public', 'index.html'));
 
         });
-
 
     };
 
